bebb3a1ea186948e48d53d9512b3b56985ff4ee9
Ximing Xing <ximingxing@gmail.com>
2020-09-19 21:32:37 +0800

    move to model floder

---
+++ convolutional_neural_networks/model/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+   Description : 
+   Author :        xxm
+"""
+import convolutional_neural_networks.model.alexnet as alexnet
+import convolutional_neural_networks.model.vgg as vgg
