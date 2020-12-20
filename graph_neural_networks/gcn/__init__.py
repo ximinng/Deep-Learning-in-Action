a3a004bd8128cc2908034fd132955c3451210054
Ximing Xing <ximingxing@gmail.com>
2020-12-20 18:35:20 +0800

    add references to gcn

--- graph_neural_networks/gcn/__init__.py
+++ graph_neural_networks/gcn/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-   Description : 
+   Description :   gcn
+   References:
+        - Semi-Supervised Classification with Graph Convolutional Networks
+        - Paper: https://arxiv.org/abs/1609.02907
+        - Code: https://github.com/tkipf/gcn
    Author :        xxm
 """
